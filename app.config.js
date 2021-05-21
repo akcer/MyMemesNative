3832b7ce44f083c6ca5a5b9255f3c21d92c7bba5
Jarek Hryniewicki <jarhryniewicki@gmail.com>
2021-05-21 14:44:30 +0200

    Set remote API Url

--- app.config.js
+++ app.config.js
@@ -2,6 +2,8 @@ export default {
   name: 'MyMemesNative',
   version: '1.0.0',
   extra: {
-    apiUrl: process.env.API_URL = 'http://192.168.8.100:3001',
+    //apiUrl: process.env.API_URL = 'http://192.168.8.100:3001',
+    apiUrl: (process.env.API_URL =
+      'https://radiant-lowlands-32082.herokuapp.com'),
   },
 };
