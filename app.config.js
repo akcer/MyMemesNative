3520e54daeb35478447d960afcae8a5c376c0ff2
Jarek Hryniewicki <jarhryniewicki@gmail.com>
2021-06-28 19:00:18 +0200

    Update API_URL

--- app.config.js
+++ app.config.js
@@ -2,8 +2,6 @@ export default {
   name: 'MyMemesNative',
   version: '1.0.0',
   extra: {
-    //apiUrl: process.env.API_URL = 'http://192.168.8.100:3001',
-    apiUrl: (process.env.API_URL =
-      'https://radiant-lowlands-32082.herokuapp.com'),
+    apiUrl: (process.env.API_URL = 'http://localhost:3001'),
   },
 };
